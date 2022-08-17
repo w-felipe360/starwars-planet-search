f09c05c9aac68978044bc8ae5797b7d6f153972d
Willian Felipe <w.felipebraz@gmail.com>
2022-08-17 16:46:38 -0300

    Hello There, iniciando o projeto

---
+++ src/context/starWarsContext.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const starWarsContext = React.createContext();
+
+export default starWarsContext;
---
+++ src/context/starWarsProvider.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class starWarsProvider extends Component {
+  render() {
+    return (
+      <div>starWarsProvider</div>
+    );
+  }
+}
+export default starWarsProvider;
